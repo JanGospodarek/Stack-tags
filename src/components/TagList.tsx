2ef8acf709317b889a88e89ad1572fdf5e6ad58a
JanGospodarek <jan.gospo@onet.pl>
2024-03-27 13:49:56 +0100

    fix sorting

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo } from "react";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import fetchTags from "../utils/fetchTags";
-import { setTags } from "../store/listSlice";
+import { setPageNumber, setTags } from "../store/listSlice";
 import compare from "../utils/compareStringsOrNumbers";
 import Tag from "./Tag";
 const TagList = () => {
@@ -11,22 +11,27 @@ const TagList = () => {
   const itemsPerPage = useAppSelector((state) => state.list.itemsPerPage);
   const sortBy = useAppSelector((state) => state.list.sortBy);
   const sorting = useAppSelector((state) => state.list.sorting);
-
+  const getTags = async () => {
+    const resTags = await fetchTags(currentPage, itemsPerPage, sorting, sortBy);
+    dispatch(setTags(resTags));
+  };
   useEffect(() => {
-    (async () => {
-      const resTags = await fetchTags(currentPage, itemsPerPage);
-      dispatch(setTags(resTags));
-    })();
+    getTags();
   }, [currentPage, itemsPerPage]);
 
-  const sorted = useMemo(
-    () => [...tags].sort((a, b) => compare(a[sortBy], b[sortBy], sorting)),
-    [tags, sortBy, sorting]
-  );
+  useEffect(() => {
+    dispatch(setPageNumber(1));
+    getTags();
+  }, [sortBy, sorting]);
+
+  //   const sorted = useMemo(
+  //     () => [...tags].sort((a, b) => compare(a[sortBy], b[sortBy], sorting)),
+  //     [tags, sortBy, sorting]
+  //   );
 
   return (
     <div className="overflow-y-scroll h-[100%] w-[100vw] p-8 flex flex-wrap gap-4">
-      {sorted.map((el, i) => (
+      {tags.map((el, i) => (
         <Tag tag={el} index={(currentPage - 1) * itemsPerPage + (i + 1)} />
       ))}
     </div>
--- src/utils/fetchTags.ts
+++ src/utils/fetchTags.ts
@@ -1,6 +1,13 @@
-const fetchTags = async (page: number, itemsPerPage: number) => {
+const fetchTags = async (
+  page: number,
+  itemsPerPage: number,
+  order: "desc" | "asc",
+  sort: "popularity" | "name"
+) => {
+  let orderBy: typeof sort | "popular" = sort;
+  if (orderBy === "popularity") orderBy = "popular";
   const res = await fetch(
-    `https://api.stackexchange.com/2.3/tags?page=${page}&pagesize=${itemsPerPage}&site=stackoverflow`
+    `https://api.stackexchange.com/2.3/tags?page=${page}&pagesize=${itemsPerPage}&order=${order}&sort=${sort}&site=stackoverflow`
   );
   const data = await res.json();
   return data.items;
